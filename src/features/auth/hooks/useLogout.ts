ba5f0a78eebaf3614fd938b3105a7b9618c3ecd3
Amirov <amirovqodir911@gmail.com>
2023-05-02 21:10:09 +0900

    fix login page 404 after logout

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import { BASE_AUTH_TOKEN, ROUTE_LOGIN } from '../../../common/constants';
 
 export const useLogout = () => {
+  const navigate = useNavigate()
   const handleLogout = () => {
     localStorage.removeItem(BASE_AUTH_TOKEN);
-    window.location.href = ROUTE_LOGIN;
+    navigate(ROUTE_LOGIN)
   };
 
   return { handleLogout };
